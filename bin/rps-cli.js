8310f547503aae1cda616359bd2d95d8a326bc99
maxxuUNC33 <maxxu@ad.unc.edu>
2023-05-07 21:28:02 -0400

    npm link

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minimist from "minimist";
+
 // rps-cli.js
 import { rps } from './lib/rpsls.js';
 import { playGameCLI } from './game-cli.js';
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import minimist from "minimist";
+
 // rpsls-cli.js
 import { rpsls } from './lib/rpsls.js';
 import { playGameCLI } from './game-cli.js';
